2e0b9ecd37147001ac73af057ff98eda5028b389
AnssetRGTecsup <130480252+AnssetRGTecsup@users.noreply.github.com>
2024-10-29 14:45:10 -0500

    Horizontal Movement X Commit

---
+++ Assets/ExampleControl.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+public class ExampleControl : MonoBehaviour
+{
+    public float xSpeed;
+    public int xDirection;
+    private Transform _componentTransform;
+    private SpriteRenderer _componentSpriteRenderer;
+    void Start()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _componentTransform = GetComponent<Transform>();
+        _componentSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+    }
+    void Movement()
+    {
+        _componentTransform.position = new Vector2(_componentTransform.position.x + xSpeed * xDirection * Time.deltaTime,
+          _componentTransform.position.y);
+        if (_componentTransform.position.x >= 7.36f)
+        {
+            xDirection = -1;
+            _componentSpriteRenderer.flipX = true;
+        }
+        else if (_componentTransform.position.x <= -7.36f)
+        {
+            xDirection = 1;
+            _componentSpriteRenderer.flipX = false;
+        }
+    }
+}
